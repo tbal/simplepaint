ec84a0f2cbd12b8614884e9adf667f4550830eb9
Tilo Baller <baller@cosmocode.de>
2020-05-30 15:35:39 +0200

    Updates

---
+++ precache-manifest.82c1e23edeb3143ceb0d9b114fb8a1c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83ef59db81d251d1768ff3fe69337a3",
+    "url": "/rma-project/index.html"
+  },
+  {
+    "revision": "075067377950f5f8f366",
+    "url": "/rma-project/static/css/main.7a060ff9.chunk.css"
+  },
+  {
+    "revision": "3cdd9f73a6f19ac3d125",
+    "url": "/rma-project/static/js/2.bb1a244f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rma-project/static/js/2.bb1a244f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075067377950f5f8f366",
+    "url": "/rma-project/static/js/main.8898b7b8.chunk.js"
+  },
+  {
+    "revision": "320b1efa83d0edbc18ec",
+    "url": "/rma-project/static/js/runtime-main.545653eb.js"
+  }
+]);
